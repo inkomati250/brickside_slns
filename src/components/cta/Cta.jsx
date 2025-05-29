c914e0227807e63c9c2eb48996d7aad58fc38874
inkomati250 <nkomatii@gmail.com>
2025-05-29 20:00:01 +0200

    cta section added

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -3,9 +3,15 @@ import './cta.css';
 
 const Cta = () => {
     return(
-        <div>
-            Cta
-        </div>
+       <div className="gpt3__cta">
+           <div className="gpt3__cta-content">
+               <p>Ready to take your business to the next level?</p>
+               <h3>Contact us today and let’s get started!</h3>
+           </div>
+           <div className="gpt3__cta-btn">
+               <button>Contact Us</button>
+           </div>
+       </div>
     )
 }
 export default Cta;
