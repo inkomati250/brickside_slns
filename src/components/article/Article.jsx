41876be214caa2eb7960f6ae99494dceb4b6218d
Ivan Nkomati <47068719+inkomati250@users.noreply.github.com>
2025-06-11 02:39:06 +0000

    Wed, Jun 11, 2025, 4:39 AM +02:00

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './article.css';
 
-
 const Article = () => {
-
-    return(
-        <div className =gpt3__blog-container_article>
-          <div className ="gpt3__blog-container_article-image">
+    return (
+        <div className="gpt3__blog-container_article">  {/* Added quotation marks */}
+            <div className="gpt3__blog-container_article-image">
+                {/* You can add an image or any content here */}
             </div>
-        <div/>
-    )
+            <div className="gpt3__blog-container_article-content">
+                <h2>Article Title</h2>
+                <p>This is a brief description of the article.</p>
+            </div>
+        </div>  {/* Correctly closed the div */}
+    );
 }
 
 export default Article;
-// This component is a placeholder for the article section of the application.
 
+// This component is a placeholder for the article section of the application.
