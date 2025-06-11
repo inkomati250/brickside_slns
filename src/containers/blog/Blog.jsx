666af983430cf83a80c713bcf207d53ad802444a
Ivan Nkomati <47068719+inkomati250@users.noreply.github.com>
2025-06-11 02:27:37 +0000

    Wed, Jun 11, 2025, 4:27 AM +02:00

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
 import './blog.css';
 
-
 const Blog = () => {
   return (
-    <div className = "gpt3__blog section__padding" id ="blog">
-      <div className ="gpt3__blog-heading">
-        <h1 className ="gradient__text">A lot is happening, <br /> We are blogging about it.</h1>
-
+    <div className="gpt3__blog section__padding" id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">A lot is happening, <br /> We are blogging about it.</h1>
       </div>
 
-      <div className= gpt3__blog-container >
-        <div className = "gpt3__blog-container_groupA">
-
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          {/* You can add blog posts or content here */}
+          <h2>Blog Post Title</h2>
+          <p>This is a brief description of the blog post.</p>
         </div>
-
-        <div className= gpt3__blog-container >
-        <div className = "gpt3__blog-container_groupA">
-
-        </div>
-
       </div>
-
     </div>
   );
 }
+
 export default Blog;
